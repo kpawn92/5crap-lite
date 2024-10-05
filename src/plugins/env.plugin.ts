6c19e1e5def8518e168c34885e34136f565107ca
Alejandro Pozo <hap921020@gmail.com>
2024-10-05 10:46:16 -0400

    fix: enviroments plugins

--- src/plugins/env.plugin.ts
+++ src/plugins/env.plugin.ts
@@ -1,4 +1,10 @@
 import * as env from "env-var";
+import { resolve } from "path";
+import * as dotenv from "dotenv";
+
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: resolve(__dirname, "/../../.env") });
+}
 
 export const envs = {
   RUT: env.get("RUT").required().asString(),
@@ -7,4 +13,5 @@ export const envs = {
   MONGO_USER: env.get("MONGO_USER").required().asString(),
   MONGO_PASS: env.get("MONGO_PASS").required().asString(),
   MONGO_DB_NAME: env.get("MONGO_DB_NAME").required().asString(),
+  NODE_ENV: env.get("NODE_ENV").default("development").asString(),
 };
