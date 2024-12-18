cf2367b6839f17b797a3c00c1a9d57b2a228a33e
Alejandro Pozo <hap921020@gmail.com>
2024-12-17 22:50:43 -0500

    dev/fix: datecalc

--- src/causes/helpers/date-calc.ts
+++ src/causes/helpers/date-calc.ts
@@ -1,4 +1,20 @@
 export const dateCalc = (dateString: string): Date => {
   const [day, month, year] = dateString.split("/").map((item) => Number(item));
-  return new Date(year, month - 1, day);
+
+  // Validar que year, month y day sean números válidos
+  if (
+    Number.isNaN(day) ||
+    Number.isNaN(month) ||
+    Number.isNaN(year) ||
+    year < 1000 ||
+    year > 9999 || // Asegurar un rango de año razonable
+    month < 1 ||
+    month > 12 ||
+    day < 1 ||
+    day > new Date(year, month, 0).getDate() // Verifica que el día esté dentro del rango del mes
+  ) {
+    return new Date(); // Retorna la fecha actual si la validación falla
+  }
+
+  return new Date(year, month - 1, day); // Retorna la fecha válida
 };
